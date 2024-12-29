b4bf4fe11cb851ff40e5b44ccf73d66979812f98
Timo Noordzee <timo.noordzee@d2t.nl>
2024-12-29 21:29:22 +0100

    Day17 part 2
    Benchmark    Mode  Cnt    Score     Error  Units
    Day17.part1  avgt   10    1,556 ±   0,266  us/op
    Day17.part2  avgt   10  829,148 ± 136,855  us/op

--- src/adventofcode/Day17.kt
+++ src/adventofcode/Day17.kt
@@ -2,7 +2,6 @@ package adventofcode
 
 import org.openjdk.jmh.annotations.*
 import java.util.concurrent.TimeUnit
-import kotlin.math.pow
 
 @State(Scope.Benchmark)
 @Fork(1)
@@ -21,46 +20,62 @@ class Day17 {
 
     @Benchmark
     fun part1(): String {
+        val a = input[0].substringAfterLast(' ').toLong()
+        val b = input[1].substringAfterLast(' ').toLong()
+        val c = input[2].substringAfterLast(' ').toLong()
         val program = input[4].substringAfter(' ').split(',').map { it.toInt() }
-        var a = input[0].substringAfterLast(' ').toInt()
-        var b = input[1].substringAfterLast(' ').toInt()
-        var c = input[2].substringAfterLast(' ').toInt()
-        var instructionPointer = 0
-
-        fun literalOperand() = program[instructionPointer++]
-
-        fun comboOperand() = when (val value = program[instructionPointer++]) {
-            in 0 until 4 -> value
-            4 -> a
-            5 -> b
-            6 -> c
-            else -> error("impossible value ($value) for combo operand")
-        }
-
-        val output = mutableListOf<Int>()
-
-        while (instructionPointer < program.size) {
-            when (program[instructionPointer++]) {
-                ADV -> a /= (2.0).pow(comboOperand()).toInt()
-                BXL -> b = b xor literalOperand()
-                BST -> b = comboOperand() and 7
-                JNZ -> if (a != 0) instructionPointer = literalOperand() else instructionPointer++
-                BXC -> {
-                    b = b xor c
-                    instructionPointer++
-                }
-                OUT -> output.add(comboOperand() and 7)
-                BDV -> b = a / (2.0).pow(comboOperand()).toInt()
-                CDV -> c = a / (2.0).pow(comboOperand()).toInt()
-            }
-        }
-
-        return output.joinToString(",")
+        return runProgram(a, b, c, program).joinToString(",")
     }
 
     @Benchmark
-    fun part2(): Int {
-        return 0
+    fun part2(): Long {
+        val b = input[1].substringAfterLast(' ').toLong()
+        val c = input[2].substringAfterLast(' ').toLong()
+        val program = input[4].substringAfter(' ').split(',').map { it.toInt() }
+
+        fun findA(target: List<Int>): List<Long> {
+            val prefixes = if (target.size == 1) listOf(0L) else findA(target.drop(1)).map { it shl 3 }
+            return prefixes.flatMap { prefix ->
+                (0 until 8).map { prefix + it }.filter { a -> runProgram(a, b, c, program) == target }
+            }
+        }
+
+        return findA(program).min()
+    }
+
+    @Suppress("NAME_SHADOWING")
+    private fun runProgram(a: Long, b: Long, c: Long, program: List<Int>): List<Int> {
+        var a = a
+        var b = b
+        var c = c
+        var instructionPointer = 0
+
+        val output = mutableListOf<Int>()
+
+        while (true) {
+            val opcode = program[instructionPointer++]
+            val operand = program[instructionPointer++]
+            val comboOperand = when (operand) {
+                in 0 until 4 -> operand.toLong()
+                4 -> a
+                5 -> b
+                6 -> c
+                else -> error("impossible value ($operand) for combo operand")
+            }
+
+            when (opcode) {
+                ADV -> a = a shr comboOperand.toInt()
+                BXL -> b = b xor operand.toLong()
+                BST -> b = comboOperand and 7
+                JNZ -> if (a != 0L) instructionPointer = operand else break
+                BXC -> b = b xor c
+                OUT -> output += (comboOperand and 7).toInt()
+                BDV -> b = a shr comboOperand.toInt()
+                CDV -> c = a shr comboOperand.toInt()
+            }
+        }
+
+        return output
     }
 
     companion object {
@@ -82,9 +97,9 @@ fun main() {
     day17.input = readInput("Day17_test_1")
     check(day17.part1().also { println("test part 1: $it") } == "4,6,3,5,6,3,5,2,1,0")
     day17.input = readInput("Day17_test_2")
-    check(day17.part2().also { println("test part 2: $it") } == 0)
+    check(day17.part2().also { println("test part 2: $it") } == 117440L)
 
     day17.input = readInput("Day17")
     check(day17.part1().also { println("part 1: $it") } == "7,4,2,5,1,4,6,0,4")
-    check(day17.part2().also { println("part 2: $it") } == 0)
+    check(day17.part2().also { println("part 2: $it") } == 164278764924605L)
 }
